af6b87787c4c9f56a17a6281f73f3b5aaed6823c
melese <meleseayen1999@gmail.com>
2024-02-26 13:37:29 +0300

    Hero section is added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("active");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var text = "Website Designer and Developer.";
+  var index = 0;
+  var speed = 120;
+
+  function typeWriter() {
+    if (index < text.length) {
+      document.getElementById("typed-text").innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
+});
