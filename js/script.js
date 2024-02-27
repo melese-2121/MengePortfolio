64dcbd7543a90d9697fe06428a2a7335841ba37e
melese <meleseayen1999@gmail.com>
2024-02-27 19:17:10 +0300

    Projects section added

--- js/script.js
+++ js/script.js
@@ -24,3 +24,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   typeWriter();
 });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
